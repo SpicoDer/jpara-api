fef55b133d56b4a012f0c7cf34d72eb8c75e139c
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-08-18 13:52:27 +0800

    Add CORS Headers

--- src/app.js
+++ src/app.js
@@ -6,5 +6,13 @@ export const app = express();
 // middlewares
 app.use(express.json());
 
+// CORS headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 // routes
 app.use("/coordinates", coordsRouter);
